d61b10bee530b24087abf0754ee6809d8fb77327
Bergis1610 <89493437+Bergis1610@users.noreply.github.com>
2021-10-06 04:04:51 +0200

    Update ExtractText.java

--- Part II/ExtractText.java
+++ Part II/ExtractText.java
@@ -9,23 +9,37 @@ import java.util.Scanner;
  * This program will use the PDFBox library from Apache Foundation to extract
  * the data for the PDF that you can download at bellarmine.edu as a text file
  * @author Emil Bjørlykke Berglund
- * @verion 1.0
+ * @version 1.0
  * Compiler Project 3
  * CS-322 Compiler Construction
  * Fall 2021
  */
 
 
+/** Note to self:
+ * To compile a program on the command line with an external jar use the following command:
+ * javac -g -d "JarFilePath" -cp "NameOfJarFile.jar" "ClassName".java
+ * Then run the program using the following command:
+ * java -cp "JarFilePath".jar;. "ClassName"
+ */
+
+
 public class ExtractText {
 
 
-    //This method is used to scan a PDF file
+    /**
+     * This method is used to scan a PDF file
+     */
+
     public static String getText(File pdfFile) throws IOException {
         PDDocument doc = PDDocument.load(pdfFile);
         return new PDFTextStripper().getText(doc);
     }
 
-    //Method for creating a file
+    /**
+     * Method for creating a file
+     * @param name
+     */
     public void CreateFile(String name){
         try {
             File myObj = new File(name);
@@ -41,7 +55,12 @@ public class ExtractText {
     }
 
 
-    //Method for using the getText() method to return the text scanned from a PDF
+    /**
+     * Method for using the getText() method to return the text scanned from a PDF
+     * @param nameOfPdf
+     * @return String
+     */
+
     public String ReadFromPDF(String nameOfPdf){
 
         String text = "";
@@ -55,8 +74,14 @@ public class ExtractText {
 
     }
 
-    //This method takes a string input, the one thats returned from the ReadFromPDF() method
-    //And writes it to a given text file.
+    /**
+     * This method takes a string input, the one thats returned from the ReadFromPDF() method
+     * And writes it to a given text file.
+     * @param fileName
+     * @param text
+     */
+
+
     public void writeToFile(String fileName, String text){
 
         try {
@@ -70,7 +95,12 @@ public class ExtractText {
         }
     }
 
-    //Method using the three previous one to extract, and write text from a pdf file to a text file.
+    /**
+     * Method using the three previous one to extract, and write text from a pdf file to a text file.
+     * @param PDFName
+     * @param fileName
+     */
+
     public void pdfExtractor(String PDFName, String fileName){
 
         CreateFile(fileName);
@@ -81,11 +111,12 @@ public class ExtractText {
     }
 
 
-    //Main method
+    //main
     public static void main(String[]args){
 
         ExtractText o = new ExtractText();
         Scanner scan = new Scanner(System.in);
+
 
         System.out.print("Please type the path of the PDF from which you want to extract the text: ");
         String PDF1 = scan.nextLine();
@@ -94,10 +125,12 @@ public class ExtractText {
 
         o.pdfExtractor(PDF1,newFile);
 
+
+
         //Strings that I used to create the files
         /*
-        String PDF1 = "C:\\Users\\Bruker\\IdeaProjects\\ByteCodeTester\\src\\CCBellarmineScheduleFall21.txt";
-        String PDF2 = "C:\\Users\\Bruker\\IdeaProjects\\ByteCodeTester\\src\\CCBellarmineScheduleSpring21.txt";
+        String PDF1 = "C:\\Users\\Bruker\\IdeaProjects\\ByteCodeTester\\src\\CCBellarmineScheduleFall21.pdf";
+        String PDF2 = "C:\\Users\\Bruker\\IdeaProjects\\ByteCodeTester\\src\\CCBellarmineScheduleSpring21.pdf";
 
         String newFile1 = "C:\\Users\\Bruker\\IdeaProjects\\ByteCodeTester\\src\\Fall21.txt";
         String newFile2 = "C:\\Users\\Bruker\\IdeaProjects\\ByteCodeTester\\src\\Spring21.txt";
@@ -106,3 +139,4 @@ public class ExtractText {
     }//end of main
 
 }//end of class
+
